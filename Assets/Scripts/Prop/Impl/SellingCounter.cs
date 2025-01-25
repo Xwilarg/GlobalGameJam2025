84fb45be2248a891d76743bea9c73daa8994274f
Xwilarg <xwilarg@protonmail.com>
2025-01-25 18:49:38 +0100

    Fix economy

--- Assets/Scripts/Prop/Impl/SellingCounter.cs
+++ Assets/Scripts/Prop/Impl/SellingCounter.cs
@@ -38,7 +38,7 @@ namespace GGJ.Prop.Impl
         public void UpdateVariation(float average)
         {
             Variation = (1f + Variation) / 2f;
-            Variation += AmountSold - average;
+            Variation += AmountSold + average;
         }
 
         public void Interact(PlayerController pc)
