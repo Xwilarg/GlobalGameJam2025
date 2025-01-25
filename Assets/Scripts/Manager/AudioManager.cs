991d72ab798f1e7645bbd079cb9b5707618b2ecf
Xwilarg <xwilarg@protonmail.com>
2025-01-25 17:26:56 +0100

    Add audio curves

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -28,15 +28,15 @@ namespace GGJ.Manager
             else if (GameManager.Instance.GamePhase == GamePhase.PriceRaise)
             {
                 var val01 = ((TimeManager.Instance.Day01 * 10f) + TimeManager.Instance.TimeWithinDay01) / 10f;
-                _bgmHappy.volume = val01 * _maxVolume;
-                _bgmNeutral.volume = _maxVolume - (val01 * _maxVolume);
+                _bgmHappy.volume = ResourceManager.Instance.GameInfo.AudioCurveIncrease.Evaluate(val01) * _maxVolume;
+                _bgmNeutral.volume = ResourceManager.Instance.GameInfo.AudioCurveDecrease.Evaluate(val01) * _maxVolume;
                 _bgmSad.volume = 0f;
             }
             else
             {
                 var val01 = ((TimeManager.Instance.Day01 * 10f) + TimeManager.Instance.TimeWithinDay01) / 10f;
-                _bgmSad.volume = val01 * _maxVolume;
-                _bgmHappy.volume = _maxVolume - (val01 * _maxVolume);
+                _bgmSad.volume = ResourceManager.Instance.GameInfo.AudioCurveIncrease.Evaluate(val01) * _maxVolume;
+                _bgmHappy.volume = ResourceManager.Instance.GameInfo.AudioCurveDecrease.Evaluate(val01) * _maxVolume;
                 _bgmNeutral.volume = 0f;
             }
         }
