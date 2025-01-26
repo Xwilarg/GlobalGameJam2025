679de302d484772518282afc682e150e402cce1e
Jean-LoupMARTIN <jlmc78@outlook.fr>
2025-01-26 15:05:11 +0100

    fix day01

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -51,14 +51,16 @@ namespace GGJ.Manager
             }
             else if (GameManager.Instance.GamePhase == GamePhase.PriceRaise)
             {
-                var val01 = ((TimeManager.Instance.Day01 * 10f) + TimeManager.Instance.TimeWithinDay01) / 10f;
+                //var val01 = ((TimeManager.Instance.Day01 * 10f) + TimeManager.Instance.TimeWithinDay01) / 10f;
+                var val01 = TimeManager.Instance.Day01 + TimeManager.Instance.TimeWithinDay01 / TimeManager.Instance.PhaseDuration;
                 _bgmHappy.volume = ResourceManager.Instance.GameInfo.AudioCurveIncrease.Evaluate(val01) * _maxVolume;
                 _bgmNeutral.volume = ResourceManager.Instance.GameInfo.AudioCurveDecrease.Evaluate(val01) * _maxVolume;
                 _bgmSad.volume = 0f;
             }
             else
             {
-                var val01 = ((TimeManager.Instance.Day01 * 10f) + TimeManager.Instance.TimeWithinDay01) / 10f;
+                //var val01 = ((TimeManager.Instance.Day01 * 10f) + TimeManager.Instance.TimeWithinDay01) / 10f;
+                var val01 = TimeManager.Instance.Day01 + TimeManager.Instance.TimeWithinDay01 / TimeManager.Instance.PhaseDuration;
                 _bgmSad.volume = ResourceManager.Instance.GameInfo.AudioCurveIncrease.Evaluate(val01) * _maxVolume;
                 _bgmHappy.volume = ResourceManager.Instance.GameInfo.AudioCurveDecrease.Evaluate(val01) * _maxVolume;
                 _bgmNeutral.volume = 0f;
